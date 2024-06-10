e9c9e5be2db8db60b16addf65cb95a2e637548ea
burtellbee <burtellbee@gmail.com>
2024-06-11 00:20:02 +0400

    fix

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
     Telegram.WebApp.ready();
+    const cloudValue = Telegram.WebApp.CloudStorage.getItem('value');
     const valueEl = document.getElementById('value');
-    Telegram.WebApp.CloudStorage.getItem('value', (v) => {
-        console.log(v);
-        valueEl.innerText = v;
-    });
+    if (cloudValue) {
+        console.log(cloudValue);
+        //valueEl.innerText = cloudValue;
+    }
     const nameContainer = document.getElementById('name');
     nameContainer.innerText = '@user';
     const formContainer = document.getElementById('form');
